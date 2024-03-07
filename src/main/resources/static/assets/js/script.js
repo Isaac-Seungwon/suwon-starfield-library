6fd86e4a1c29691b507288a829fb56f990bd7643
Isaac-Seungwon <zhzkdkrak@naver.com>
2024-03-07 17:47:01 +0900

    Refactor: Convert functions to arrow functions
    - Updated function syntax to utilize arrow function expressions
    - No change in functionality, purely syntactic refactoring

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -6,7 +6,7 @@ const bookForm = $('#bookForm');
 const bookTable = $('#bookTable').find('tbody');
 
 /* 데이터를 렌더링하는 함수 */
-function renderBooks(books) {
+const renderBooks = (books) => {
     // 테이블 내용 초기화
     bookTable.empty();
 
@@ -25,18 +25,17 @@ function renderBooks(books) {
         // 행을 테이블에 추가
         bookTable.append(row);
     });
-}
+};
 
 /* 폼 제출 이벤트 리스너 */
-bookForm.submit(function(event) {
+bookForm.submit((event) => {
     event.preventDefault(); // 폼의 기본 동작 방지
-    let authorId;
 
     // 책 데이터 수집
     const bookData = {
         id: $('#bookId').val(),
         title: $('#title').val(),
-        author: { id: authorId, name: $('#author').val() }, // 작가 정보
+        author: { id: null, name: $('#author').val() }, // 작가 정보
         price: $('#price').val()
     };
 
@@ -44,16 +43,12 @@ bookForm.submit(function(event) {
     saveBook(bookData);
 
     // HTML 요소 초기화
-    $('#bookId').val('');
-    $('#title').val('');
-    $('#author').val('');
-    $('#price').val('');
-
-    $(this).trigger('reset'); // 폼 초기화
+    $('#bookId, #title, #author, #price').val('');
+    bookForm.trigger('reset'); // 폼 초기화
 });
 
 /* 책 저장하는 함수, 책 수정하는 함수 */
-function saveBook(book) {
+const saveBook = (book) => {
     let url = '/starfield/books';
     let method = 'POST';
 
@@ -69,37 +64,38 @@ function saveBook(book) {
         url: url,
         contentType: 'application/json',
         data: JSON.stringify(book),
-        success: function(data) {
+        success: () => {
             // 책 저장 성공 시 목록 다시 불러오기
             getAllBooks();
         },
-        error: function(xhr, status, error) {
+        error: (xhr, status, error) => {
             console.error('Error:', error);
         }
     });
-}
+};
 
 /* 책 수정할 때 해당 책 데이터 불러오는 함수 */
-function editBook(id) {
+const editBook = (id) => {
     // AJAX를 통해 책 정보 요청
     $.ajax({
         type: 'GET',
         url: `/starfield/books/${id}`,
-        success: function(data) {
+        success: (data) => {
             // 폼에 책 데이터 채우기
+            $('#bookId, #title, #author, #price').val('');
             $('#bookId').val(data.id);
             $('#title').val(data.title);
             $('#author').val(data.author.name);
             $('#price').val(data.price);
         },
-        error: function(xhr, status, error) {
+        error: (xhr, status, error) => {
             console.error('Error:', error);
         }
     });
-}
+};
 
 /* 책 삭제하는 함수 */
-function deleteBook(id) {
+const deleteBook = (id) => {
     // 사용자에게 확인을 받기 위한 경고창 띄우기
     if (confirm("정말로 삭제하시겠습니까?")) {
         // 사용자가 확인을 클릭한 경우 책 삭제 요청 보내기
@@ -114,26 +110,21 @@ function deleteBook(id) {
         })
         .catch(error => console.error('Error:', error));
     }
-}
+};
 
 /* 페이지 로드 시 책 목록 가져오기 */
-function getAllBooks() {
+const getAllBooks = () => {
     // 책 목록 요청
     fetch('/starfield/books/all')
     .then(response => response.json())
-    .then(data => {
-        // 책 목록 렌더링
-        renderBooks(data);
-    })
+    .then(renderBooks)
     .catch(error => console.error('Error:', error));
-}
+};
 
 /* 가격에 쉼표 추가 */
-function formatPrice(price) {
+const formatPrice = (price) => {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+};
 
 /* 페이지 로드 시 책 목록 초기화 */
-$(document).ready(function() {
-    getAllBooks();
-});
+$(document).ready(getAllBooks);
